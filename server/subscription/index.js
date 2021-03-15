b5a017c00d38f7079aeb3e5e728b2c5af63a1648
“lyons” <lyons124@uw.edu>
2021-03-15 13:45:57 +0800

    fix endpoint

--- server/subscription/index.js
+++ server/subscription/index.js
@@ -17,7 +17,7 @@ const connect = () => {
 connect();
 
 app.listen(port, "", () => {
-    console.log(`server is listening ${port}`);
+    console.log(`server is listening ${port}, subscribe obj: ${Subscribe}`);
 })
 const {
     subscriptionGetHandler,
